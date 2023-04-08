de1ee51172c6003b3583ced185a61e5d818376b5
kevin <kevin.pernet73@gmail.com>
2023-04-08 13:47:47 +0200

    merge

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const router = require("express").Router();
 const apiRoute = require("./api");
 const authRoute = require("./auth");
@@ -8,17 +7,5 @@ router.use("/api", apiRoute);
 router.use("/auth", authRoute);
 router.use("/users", userRoutes);
 router.get("/", (req, res) => res.json("Hello world"));
-=======
-const router = require('express').Router();
-const apiRoute = require("./api")
-const authRoute = require("./auth")
-const helloRoute = require("./hello")
-const userRoutes = require("./users")
-
-router.use("/api", apiRoute)
-router.use("/auth", authRoute)
-router.use("/hello", helloRoute)
-router.use("/users",userRoutes)
->>>>>>> 57b6178961028cdf08d47257e37b5bd20d1adcdd
 
 module.exports = router;
