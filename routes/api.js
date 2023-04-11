7f3c7ef468dfb6e0682e42bf30d54c8f4fa37f56
kevin <kevin.pernet73@gmail.com>
2023-04-11 19:25:55 +0200

    Test comunication with backend

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 
-router.get("/",(req, res) => {
-  res.json("api")
-})
+router.post("/", (req, res) => {
+  const body = req.body;
+  res.json(body);
+});
 
 module.exports = router;
