24baaf691b426cf8e81a1daf349710b3e23b2c82
kevin <kevin.pernet73@gmail.com>
2023-04-11 17:22:23 +0200

    Test comunication with backend

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
