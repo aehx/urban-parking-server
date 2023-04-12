a71de5bbd56cfe98ab5b2912b8a7884cb7497f93
kevin <kevin.pernet73@gmail.com>
2023-04-12 16:56:14 +0200

    Test comunication with backend

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,7 +1,15 @@
+const {
+  findUserPerEmail,
+  createUser,
+  updateUserToken,
+} = require("../queries/user.queries");
+const { createJwtToken } = require("../config/jwt.config");
+
 exports.test = async (req, res, next) => {
   const {email,password} = req.body;
   try {
-      res.json({email,password});
+    const user = await findUserPerEmail(email);
+      res.json({user});
   } catch (err) {
     res.json("notnot");
     // next(err);
