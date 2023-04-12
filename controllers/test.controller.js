a74635a264515f58ac6256c43cd28e63b705c869
kevin <kevin.pernet73@gmail.com>
2023-04-12 15:54:49 +0200

    Test comunication with backend

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,7 +1,11 @@
 exports.test = async (req, res, next) => {
   const body = req.body;
   try {
-    res.json(body);
+    if(body){
+      res.json(body);
+    } else{
+      res.json("body");
+    }
   } catch (err) {
     res.json("notnot");
     // next(err);
