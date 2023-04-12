f71ca3944a2469b4a86b3f41876e9925491939e6
kevin <kevin.pernet73@gmail.com>
2023-04-12 14:33:58 +0200

    Test comunication with backend

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,8 +1,28 @@
 exports.test = async (req, res, next) => {
   try {
     const body = req.body;
-    res.json(body);
+    const { email, password } = req.body;
+    res.json(email,password);
+    const user = await findUserPerEmail(email);
+    if (!user) {
+      res.json({ error: "user not found", user: user });
+    }
+    const match = await user.comparePassword(password);
+    if (match) {
+      const token = await createJwtToken(user._id);
+      await updateUserToken(user._id, token);
+      const returnedUser = {
+        username: user.username,
+        favorites: user.favorites,
+        token,
+      };
+      res.status(200).json(returnedUser);
+    } else {
+      res.status(400).json({ error: "Wrong email or password" });
+    }
+    // res.json(body);
   } catch (err) {
     next(err);
   }
 };
+
