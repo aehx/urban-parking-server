1bb60278a786e749bd6c35457e6a764e2f2fc4b0
kevin <kevin.pernet73@gmail.com>
2023-04-16 17:26:16 +0200

    add token features for favorites

--- config/jwt.config.js
+++ config/jwt.config.js
@@ -9,27 +9,26 @@ exports.createJwtToken = (user)=>{
 
 exports.verifyToken = async (req,res,next)=>{
   if(req.headers && req.headers.authorization){
-    res.json("ok")
-  // const token = req.headers.authorization.split(" ")[1];
-  // try {
-  //     const decoded = jwt.verify(token,secret);
-  //     const user = await findUserPerId(decoded.sub);
-  //     if(!user){
-  //       res.status(401).json({error:"unauthorized access"})
-  //     }else{
-  //       req.user = user
-  //       next()
-  //     }
-  // } catch (e) {
-  //   switch(e.name){
-  //     case "JsonWebTokenError":
-  //       return res.status(401).json({result : false,error:"unauthorized access"});
-  //       case "TokenExpiredError":
-  //         return res.status(401).json({result : false,error:"Session expired, please sign in again"});
-  //   }
-  //   res.json({error:"Server error"})
-  // }
-  //  } else {
-  //   res.status(401).json({error:"unauthorized access"})
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+      const decoded = jwt.verify(token,secret);
+      const user = await findUserPerId(decoded.sub);
+      if(!user){
+        res.status(401).json({error:"unauthorized access"})
+      }else{
+        req.user = user
+        next()
+      }
+  } catch (e) {
+    switch(e.name){
+      case "JsonWebTokenError":
+        return res.status(401).json({result : false,error:"unauthorized access"});
+        case "TokenExpiredError":
+          return res.status(401).json({result : false,error:"Session expired, please sign in again"});
+    }
+    res.json({error:"Server error"})
+  }
+   } else {
+    res.status(401).json({error:"unauthorized access"})
   }
 }
