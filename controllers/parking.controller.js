fdbf8c81e33142c985cb55d5d11ba28d82b4faeb
kevin <kevin.pernet73@gmail.com>
2023-04-16 21:22:43 +0200

    start to build favorite screen and favorites features

--- controllers/parking.controller.js
+++ controllers/parking.controller.js
@@ -74,11 +74,11 @@ exports.updatefavorites = async (req, res, next) => {
 };
 
 exports.getUserFavorites = async (_req,res,next)=>{
-  const {email} = req.params;
+  // const {email} = req.params;
   try {
+    res.json("ok");
     // const user = await findUserPerEmail(email);
     // const userFavorites = user.favorites;
-    res.json(email);
   } catch (error) {
     next(e)
   }
