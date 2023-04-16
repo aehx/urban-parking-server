d250dcd49aea6220dd67e2029caca44ca15a0c60
kevin <kevin.pernet73@gmail.com>
2023-04-16 17:43:20 +0200

    test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,6 @@ const { verifyToken } = require("../config/jwt.config");
 const { signout } = require("../controllers/user.controller");
 const router = require("express").Router();
 
-router.post("/signout", signout);
+router.post("/signout", verifyToken, signout);
 
 module.exports = router;
