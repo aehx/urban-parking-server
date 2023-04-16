0f6db415abb599e63ae5424fa2c2a1d73f0da139
kevin <kevin.pernet73@gmail.com>
2023-04-16 19:55:39 +0200

    start favorite routes

--- queries/parking.query.js
+++ queries/parking.query.js
@@ -3,9 +3,9 @@ const User = require("../database/models/user.model");
 exports.updateFavoriteParking = async (userId, parkingName) => {
   try {
     const update = await User.findById(userId);
-    const include = update.favorites.includes(parkingName);
-      // ? { $pull: { favorites: parkingName } }
-      // : { $push: { favorites: parkingName } };
+    const include = update.favorites.includes(parkingName)? { $pull: { favorites: parkingName } }
+    : { $push: { favorites: parkingName } };
+      
 
     const updatedUserFavorite = await User.findOneAndUpdate(
       {_id : userId},
