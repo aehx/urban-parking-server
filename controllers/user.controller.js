ee1ba22f69f7e4f0aa2b40340855ac9916e3d6ba
kevin <kevin.pernet73@gmail.com>
2023-04-18 11:05:43 +0200

    jwt test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -70,7 +70,7 @@ exports.signout = async (req, res, next) => {
 exports.removeToken = async (req, res,next) => {
   const {email} = req.body;
   try {
-    await findUserPerEmail({"local.email" : email})
+    // await findUserPerEmail({"local.email" : email})
     res.status(200).json("token removed successfully")
   } catch (e) {
     next(e)
