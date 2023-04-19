40f1d0aa6d21e9d1aa46e8ff704eb23467ffd267
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:21:16 +0200

    remove unused route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
+const apiRoute = require("./api.routes");
 const authRoute = require("./auth.routes");
 const userRoutes = require("./users.routes");
 const parkingRoutes = require("./parking.routes");
 
+router.use("/api", apiRoute);
 router.use("/auth", authRoute);
 router.use("/users", userRoutes);
 router.use("/parking",parkingRoutes)
