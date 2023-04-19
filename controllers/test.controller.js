75fe879967346754c4a5afe928f4f47130567a7d
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:05:01 +0200

    remove unused route

--- controllers/test.controller.js
+++ controllers/test.controller.js
@@ -1,30 +1,30 @@
-// const {
-//   findUserPerEmail,
-//   updateUserToken,
-// } = require("../queries/user.queries");
-// const { createJwtToken } = require("../config/jwt.config");
+const {
+  findUserPerEmail,
+  updateUserToken,
+} = require("../queries/user.queries");
+const { createJwtToken } = require("../config/jwt.config");
 
-// exports.test = async (req, res, next) => {
-//   const {email,password} = req.body;
-//   try {
-//     const user = await findUserPerEmail(email);
-//     if (!user) {
-//       res.json({ error: "user not found", user: user });
-//     }
-//     const match = await user.comparePassword(password);
-//     if (match) {
-//       const token = await createJwtToken(user._id);
-//       await updateUserToken(user._id, token);
-//       const returnedUser = {
-//         username: user.username,
-//         favorites: user.favorites,
-//         token,
-//       };
-//       res.status(200).json(returnedUser);
-//     } else {
-//       res.status(200).json({ error: "Wrong email or password" });
-//     }
-//   } catch (err) {
-//     next(err);
-//   }
-// };
+exports.test = async (req, res, next) => {
+  const {email,password} = req.body;
+  try {
+    const user = await findUserPerEmail(email);
+    if (!user) {
+      res.json({ error: "user not found", user: user });
+    }
+    const match = await user.comparePassword(password);
+    if (match) {
+      const token = await createJwtToken(user._id);
+      await updateUserToken(user._id, token);
+      const returnedUser = {
+        username: user.username,
+        favorites: user.favorites,
+        token,
+      };
+      res.status(200).json(returnedUser);
+    } else {
+      res.status(200).json({ error: "Wrong email or password" });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
