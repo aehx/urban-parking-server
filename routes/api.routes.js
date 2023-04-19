0a39909177996fbee1c9e56ee23a7dab58752950
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:39:04 +0200

    test

--- routes/api.routes.js
+++
@@ -1,9 +0,0 @@
-// const { test } = require("../controllers/test.controller.js");
-
-// const router = require("express").Router();
-
-// router.post("/", (req, res) => {
-//   res.json("ok")
-// });
-
-// module.exports = router;
