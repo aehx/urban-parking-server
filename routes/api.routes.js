d4047511e51465760c2d3275da493b7c8f58122f
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:09:12 +0200

    remove unused route

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,9 +1,7 @@
-const { test } = require("../controllers/test.controller.js");
-
 const router = require("express").Router();
 
 router.get("/", (req,res)=>{
-  res.json("success")
+  res.status(200).json("success")
 });
 
 module.exports = router;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const { login, signup } = require("../controllers/user.controller");
-const { test } = require("../controllers/test.controller");
 const {
   checkSignupField,
   checkLoginField,
@@ -9,6 +8,5 @@ const {
 
 router.post("/login",checkLoginField, validationResult, login);
 router.post("/signup", checkSignupField, validationResult, signup);
-router.post("/",checkLoginField, validationResult, test);
 
 module.exports = router;
