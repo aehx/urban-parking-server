97a0f6d3b075894b876684ea0cc47170f3828f2c
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:23:30 +0200

    remove unused route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,8 @@
 const { verifyToken } = require("../config/jwt.config");
-const {updateFavorites} = require("../controllers/user.controller")
 const { signout,removeToken } = require("../controllers/user.controller");
 const router = require("express").Router();
 
 router.post("/signout", verifyToken, signout);
 router.put("/removeToken",removeToken);
-router.put("/favorites",verifyToken, updateFavorites);
 
 module.exports = router;
--- routes/parking.routes.js
+++ routes/parking.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
+const { verifyToken } = require("../config/jwt.config");
 const {
   getParking,
+  updatefavorites,
   getUserFavorites
 } = require("../controllers/parking.controller");
 
 router.get("/", getParking);
 router.get("/userFavorites/:email",getUserFavorites);
-
+router.put("/favorites",verifyToken, updatefavorites);
 
 module.exports = router;
