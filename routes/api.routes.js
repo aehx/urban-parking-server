4b4212411595fbf13153626c7165e0a4f7f0b6ba
kevin <kevin.pernet73@gmail.com>
2023-04-19 17:10:06 +0200

    remove unused route

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,7 +1,7 @@
-const router = require("express").Router();
+// const router = require("express").Router();
 
-router.get("/", (req,res)=>{
-  res.status(200).json("success")
-});
+// router.get("/", (req,res)=>{
+//   res.status(200).json("success")
+// });
 
-module.exports = router;
+// module.exports = router;
